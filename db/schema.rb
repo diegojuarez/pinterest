a4b53dd338ac6b4f8d8f126ac1997d312434c79a
Diego <diego.utng2015@gmail.com>
2017-02-18 21:25:35 -0600

    add name

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218201137) do
+ActiveRecord::Schema.define(version: 20170219031322) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "photo"
     t.string   "descripcion"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "name"
   end
 
 end
